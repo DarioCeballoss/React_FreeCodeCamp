62f744e3b8ad78b04a529e9d830943e8159ce8f9
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-06 19:02:39 -0300

    contador usando setState

--- mi-app/src/index.js
+++ mi-app/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React , { useState } from 'react';
 import ReactDOM from 'react-dom';
 //import './style.css';
 
-class Counter extends React.Component {
-  state = {
-    counter: 0
-  }
-  increment = () => {
-    this.setState({counter: this.state.counter+1});
-  }
-  render() {
+  function Counter() {
+    const [counter, setCounter] = useState(0);
+  
+    function increment() {
+      setCounter(counter+1);
+    }
+  
     return <div>
-    <p>{this.state.counter}</p>
-    <button onClick={this.increment}>Increment</button>
+    <p>{counter}</p>
+    <button onClick={increment}>
+      Increment
+    </button>
     </div>;
-  }
-}
+  } 
 
 const el = <Counter />; 
 ReactDOM.render(
