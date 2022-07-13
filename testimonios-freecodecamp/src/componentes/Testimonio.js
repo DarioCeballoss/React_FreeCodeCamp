26696443ffdf3cc0b906d5fb92655a371afd0623
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-13 00:59:53 -0300

    primera vista

--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 function Testimonio(){
-  return(
-    <div className='contenedor-Testimonio'>
+  return (
+    <div>
+      <div className='contenedor-Testimonio'>
       <img
-      className='imagen-testimonio'
-      src={require('../imagenes/testimonio-emma.png')}
-      alt='Foto de Emma'
-      />
+        className='imagen-testimonio'
+        src={require('../imagenes/testimonio-emma.png')}
+        alt='Foto de Emma' />
+      </div>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Emma Bostain en Suecia</p>
+        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
+        <p className='texto-testimonio'>"Siempre me consto aprender JS"</p>
+      </div>
     </div>
   );
 
 }
+
+export default Testimonio;
