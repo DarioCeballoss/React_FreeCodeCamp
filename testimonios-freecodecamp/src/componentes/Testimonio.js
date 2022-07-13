c269ab9146ac8611e41c2dcd610cc0dea4c15aa4
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-13 00:29:23 -0300

    acomodando los componentes

---
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Testimonio(){
+  return(
+    <div className='contenedor-Testimonio'>
+      <img
+      className='imagen-testimonio'
+      src={require('../imagenes/testimonio-emma.png')}
+      alt='Foto de Emma'
+      />
+    </div>
+  );
+
+}
