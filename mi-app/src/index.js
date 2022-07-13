5b7154d334e7617f5680faf59dd82f5888da39f2
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-13 00:09:11 -0300

    primer commit testimonios

--- mi-app/src/index.js
+++ mi-app/src/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './style.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class Counter extends React.Component {
-  state = {
-    counter: 0
-  }
-  increment = () => {
-    this.setState({counter: this.state.counter+1});
-  }
-  componentDidMount() {
-    this.setState({counter: 42});
-  }
-  render() {
-    return <div>
-    <p>{this.state.counter}</p>
-    <button onClick={this.increment}>Increment</button>
-    </div>;
-  }
-}
-
-const el = <Counter />; 
 ReactDOM.render(
-  el, 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals(); 
