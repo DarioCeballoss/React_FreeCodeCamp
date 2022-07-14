366acc386d852e8b4ad8dda1d4eeef51ca8cafd6
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-14 01:21:46 -0300

    PROPS harcodeo de datos 3

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -7,13 +7,31 @@ function App() {
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre free CodeCamp:</h1>
-        <Testimonio 
-        nombre='Emma Bostain'
-        pais='Suecia'
-        imagen=''
-        cargo='Ingeniera de Software'
-        empresa='Spotify'
-        testimonio='Siempre me consto aprender JS
+        <Testimonio
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='shawn'
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='Siempre me consto aprender JS
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, iusto aliquid quisquam eveniet doloremque, aspernatur reprehenderit vero vitae mollitia, illo laudantium? Ipsum repudiandae porro vel excepturi, laboriosam a neque quo.'
+        />
+        <Testimonio
+          nombre='Sarah Chima'
+          pais='Ningeria'
+          imagen='sarah'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='Siempre me consto aprender JS
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, iusto aliquid quisquam eveniet doloremque, aspernatur reprehenderit vero vitae mollitia, illo laudantium? Ipsum repudiandae porro vel excepturi, laboriosam a neque quo.'
+        />
+        <Testimonio
+          nombre='Emma Bostain'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='Siempre me consto aprender JS
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, iusto aliquid quisquam eveniet doloremque, aspernatur reprehenderit vero vitae mollitia, illo laudantium? Ipsum repudiandae porro vel excepturi, laboriosam a neque quo.'
         />
       </div>
