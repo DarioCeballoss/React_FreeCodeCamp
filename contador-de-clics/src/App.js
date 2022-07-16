806ae2abed1f71683bb83b5c6be37995f40c64f9
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-16 20:24:47 -0300

    estetica y harcodeo de datos de manera desestructurada

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+import './estilos-css/Boton.css';
+import Contador from './componentes/Contador';
 
 function App() {
+//funcion
+const manejarClic = () => {
+  console.log("Clic");
+}
+//funcion ES6
+const reiniciarClic = () => {
+  console.log("reiniciar");
+}
+
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -12,7 +25,9 @@ function App() {
         />
       </div>
       <div className='contenedor-proncipal'>
-
+        <Contador  numClics='5' />
+        <Boton texto= 'Clic' esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton texto= 'Reinicar' esBotonDeClic={false} manejarClic={reiniciarClic}/>
       </div>
     </div>
   );
