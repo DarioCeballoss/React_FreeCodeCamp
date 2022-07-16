50292ed4b5942b9e579611577f1bc9fde8c050ed
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-16 01:29:25 -0300

    componente boton OK

--- contador-de-clics/src/componentes/Boton.js
+++ contador-de-clics/src/componentes/Boton.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-function Boton(){
-    return(
+function Boton({ texto, esBotonDeClic, manejarClic }) {
+    return (
+        <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+            onClick={ manejarClic } >
+            {texto}
 
-        
+        </button>
+
     );
 }
+
+export default Boton;
