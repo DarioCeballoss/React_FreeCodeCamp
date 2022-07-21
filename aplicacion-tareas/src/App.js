453bcac48275cd7d8df9f14be2063484c4ea25a7
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-21 17:34:11 -0300

    estructura del proyecto mas componente Tarea

--- aplicacion-tareas/src/App.js
+++ aplicacion-tareas/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Tarea from './componente/Tarea';
+import logofree from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
- React
+    <div className='aplicacion-tareas'>
+
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={logofree}
+          className='freecodecamp-logo'
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <Tarea texto='Aprender react' />
+      </div>
     </div>
   );
 }
