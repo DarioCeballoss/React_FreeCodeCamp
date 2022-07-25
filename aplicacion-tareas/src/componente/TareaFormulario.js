97113c772f509604f65a3272510de26df8aa6329
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-07-25 00:31:02 -0300

    Composicion Final parte 4 UUID

--- aplicacion-tareas/src/componente/TareaFormulario.js
+++ aplicacion-tareas/src/componente/TareaFormulario.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import '../estilos/TareaFormulario.css'
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function TareaFormulario(props) {
 
     const [input, setInput] = useState('');
 
     const manejarCambio = e => {
-        setInput(e.target.value); //toma el valor de lo que se escribe
+        setInput(e.target.value); //toma el valor de lo que se escribe MANEJA EL CAMBIO
     }
 
     const manejarEnvio = e => {
+        e.preventDefault(); // Para que no se actualice cuando se envia el formulario
+
         const tareaNueva = {
-            id: '561582',
-            texto: "hola"
+            id: uuidv4(),
+            texto: input,
+            completada: false
         }
+        props.onSubmit(tareaNueva);
     }
 
     return (
-        <form>
+        <form
+            className='tarea-formulario'
+            onSubmit={manejarEnvio} >
+
             <input
                 className='tarea-input'
                 type='text'
